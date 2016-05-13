84c0e20c8b1ab6d3f454471e12999087b5a1408f
Charlie Sharpsteen <source@sharpsteen.net>
2016-05-12 18:59:06 -0700

    (PE-15434) Mock MEEP support in test pre-suite
    This patch adds a step to the module installation part of the Beaker pre-suite
    which mocks up the directory structure created by the MEEP installer. Once the
    beaker-pe library supports the MEEP installer, this test step can be reverted.

--- tests/beaker/pre-suite/01_module_install.rb
+++ tests/beaker/pre-suite/01_module_install.rb
@@ -2,6 +2,21 @@ test_name 'PE-15434 - - Install pe_support_script Module' do
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '../../../'))
   meep_module_dir = '/opt/puppetlabs/server/data/enterprise/modules'
 
+  # NOTE: This step is a temporary workaround for MEEP installation not being
+  # supported by beaker-pe yet and can be removed once MEEP support is merged.
+  step 'Mock MEEP' do
+    hosts.each do |host|
+      installer_dir = "#{host['working_dir']}/#{host['dist']}"
+
+      on(host, "mkdir -p #{meep_module_dir}")
+      on(host, puppet("module install #{installer_dir}/modules/*pe_manager*",
+        modulepath: meep_module_dir))
+
+      # Remove legacy PE support script.
+      on(host, 'rm -rf /opt/puppetlabs/bin/puppet-enterprise-support /opt/puppetlabs/server/share/installer/utilities')
+    end
+  end
+
   step 'Install pe_support_script Module' do
     # Ensure any module packaged with the installer is removed.
     on(hosts, "rm -rf #{meep_module_dir}/pe_support_script")
