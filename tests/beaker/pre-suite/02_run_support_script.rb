ca095b84cebd9471ea43cd66eaf7dd55c275fa1f
Charlie Sharpsteen <chuck@puppet.com>
2016-11-08 16:24:52 +0000

    (maint) Update tests for Solaris platforms
    This patch updates the support script test suite so that output can be
    extracted on Solaris platforms where tar lacks flags for handling compressed
    input.

--- tests/beaker/pre-suite/02_run_support_script.rb
+++ tests/beaker/pre-suite/02_run_support_script.rb
@@ -5,7 +5,9 @@ test_name 'PE-15434 - - Run Support Script' do
       output_tarball = result.stdout.match(/^Support data is located at (.*)$/).captures.first
 
       stage_dir = create_tmpdir_on(host)
-      on(host, "tar xzf #{output_tarball} -C #{stage_dir}")
+      # Using a pipeline accomodates SUN tar, which doesn't support the z flag
+      # for unzipping tarballs or the -C flag when extracting.
+      on(host, "cd #{stage_dir} && gunzip -c #{output_tarball}|tar xf -")
 
       # Save path to extracted data in host object so that tests
       # can inspect it.
