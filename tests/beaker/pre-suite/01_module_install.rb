b473a0aaf63da4b2d21acdb1f7954edd94676b17
Charlie Sharpsteen <chuck@puppet.com>
2017-06-09 16:38:56 -0700

    (maint) Backport module test setup from 2017.2.x
    Starting with 2016.4.6, the pe-modules package has been added to the LTS
    install. This means an additional copy of the support script is present in:
    
        /opt/puppetlabs/puppet/modules
    
    This patch backports acceptance test setup steps from the 2017.2.x branch
    that deal with this extra path.

--- tests/beaker/pre-suite/01_module_install.rb
+++ tests/beaker/pre-suite/01_module_install.rb
@@ -1,15 +1,32 @@
+require 'beaker/host_prebuilt_steps'
+
 test_name 'PE-15434 - - Install pe_support_script Module' do
+  extend Beaker::HostPrebuiltSteps
+
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '../../../'))
+  puppet_module_dir = '/opt/puppetlabs/puppet/modules'
   meep_module_dir = '/opt/puppetlabs/server/data/enterprise/modules'
+  controllers = hosts.reject {|h| not_controller(h) }
 
-  step 'Install pe_support_script Module' do
-    # Ensure any module packaged with the installer is removed.
-    on(hosts, "rm -rf #{meep_module_dir}/pe_support_script")
-
-    install_dev_puppet_module(
+  step 'Re-install module from project source' do
+    copy_module_to(controllers + hosts_as('compile_master'),
       module_name: 'pe_support_script',
       source: proj_root,
-      target_module_path: meep_module_dir,
-    )
+      target_module_path: puppet_module_dir)
+
+    # Stub out the checksum file to prevent `puppet module changes` from
+    # throwing an error.
+    copy_file_to_remote(controllers + hosts_as('compile_master'),
+      [puppet_module_dir, 'pe_support_script', 'checksums.json'].join('/'),
+      '{}')
+
+    copy_module_to(controllers,
+      module_name: 'pe_support_script',
+      source: proj_root,
+      target_module_path: meep_module_dir)
+  end
+
+  step 'Distribute module to agents via pluginsync' do
+    on(agents, puppet("plugin download"))
   end
 end
