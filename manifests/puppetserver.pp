32d65def4c0b422be223b3d36ce71e183f5609cf
daniel5119 <dp20@hotmail.co.uk>
2018-06-26 11:46:43 +0100

    Update to Metrics Collector as per SUP-346

--- manifests/puppetserver.pp
+++ manifests/puppetserver.pp
@@ -17,5 +17,31 @@ class puppet_metrics_collector::puppetserver (
     hosts         => $hosts,
     metrics_port  => $port,
   }
+  
+  if $facts['pe_server_version'] < 2018.1.0 {
+  
+    Pe_metric_curl_cron_jobs::Pe_metric <| title == 'puppetserver' |> {
+      additional_metrics => [
+        { 'name' => 'compiler.find_node',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.compiler.find_node" },
+        { 'name' => 'puppetdb.query',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.query" },
+        { 'name' => 'puppetdb.resource.search',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.resource.search" },
+        { 'name' => 'puppetdb.facts.encode',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.facts.encode" },
+        { 'name' => 'puppetdb.command.submit.replace facts',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.command.submit.replace facts" },
+        { 'name' => 'puppetdb.catalog.munge',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.catalog.munge" },
+        { 'name' => 'puppetdb.command.submit.replace catalog',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.command.submit.replace catalog" },
+        { 'name' => 'puppetdb.report.convert_to_wire_format_hash',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.report.convert_to_wire_format_hash" },
+        { 'name' => 'puppetdb.command.submit.store report',
+          'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.command.submit.store report" },
+      ],
+    }
+  }
 
 }
