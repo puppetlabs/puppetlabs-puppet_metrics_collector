dd675d10874cdfafa464e4f589ded7c866bb444d
daniel5119 <dp20@hotmail.co.uk>
2018-07-25 02:36:44 +0100

    Sup-346 update to include puppetserver metrics for versions < 2018.1.0 (#3)
    This patch updates the Puppet Server metrics collection to include measurements of Classifier and PuppetDB operations on PE versions older than 2018.1. Requires the `metrics_webservice_enabled` parameter of the `puppet_enterprise::master::puppetserver` class to be set to `true`.

--- manifests/puppetserver.pp
+++ manifests/puppetserver.pp
@@ -12,10 +12,35 @@ class puppet_metrics_collector::puppetserver (
     retention_days => $retention_days,
   }
 
+  if versioncmp($facts['pe_server_version'], '2018.1.0') < 0 {
+    $additional_metrics = [
+      { 'name' => 'compiler.find_node',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.compiler.find_node" },
+      { 'name' => 'puppetdb.query',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.query" },
+      { 'name' => 'puppetdb.resource.search',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.resource.search" },
+      { 'name' => 'puppetdb.facts.encode',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.facts.encode" },
+      { 'name' => 'puppetdb.command.submit.replace facts',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.command.submit.replace facts" },
+      { 'name' => 'puppetdb.catalog.munge',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.catalog.munge" },
+      { 'name' => 'puppetdb.command.submit.replace catalog',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.command.submit.replace catalog" },
+      { 'name' => 'puppetdb.report.convert_to_wire_format_hash',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.report.convert_to_wire_format_hash" },
+      { 'name' => 'puppetdb.command.submit.store report',
+        'url'  => "puppetserver:name=puppetlabs.${::hostname}.puppetdb.command.submit.store report" },
+      ]
+  } else {
+     $additional_metrics = []
+  }
+
   puppet_metrics_collector::pe_metric { 'puppetserver' :
     metric_ensure => $metrics_ensure,
     hosts         => $hosts,
     metrics_port  => $port,
+    additional_metrics => $additional_metrics,
   }
-
 }
