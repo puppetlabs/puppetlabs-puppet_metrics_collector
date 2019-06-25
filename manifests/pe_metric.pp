477ed21d1297e80ec40098eb283961e75cee5704
Helen Campbell <helen@puppet.com>
2019-06-25 15:24:57 +0100

    Parameter to override entire metrics command

--- manifests/pe_metric.pp
+++ manifests/pe_metric.pp
@@ -11,6 +11,7 @@ define puppet_metrics_collector::pe_metric (
   Array[Hash]               $additional_metrics = [],
   Boolean                   $ssl                = true,
   Optional[Puppet_metrics_collector::Metrics_server] $metrics_server_info = undef,
+  Optional[String]          $override_metrics_command = undef,
 ) {
 
   $metrics_output_dir = "${output_dir}/${metrics_type}"
@@ -41,32 +42,36 @@ define puppet_metrics_collector::pe_metric (
   $script_file_name = "${scripts_dir}/${metric_script_file}"
   $conversion_script_file_name = "${scripts_dir}/json2timeseriesdb"
 
-  $metrics_base_command = "${script_file_name} --metrics_type ${metrics_type} --output-dir ${metrics_output_dir}"
+  if empty($override_metrics_command){
+    $metrics_base_command = "${script_file_name} --metrics_type ${metrics_type} --output-dir ${metrics_output_dir}"
 
-  if !empty($metrics_server_info) {
-    $metrics_server_hostname = $metrics_server_info['hostname']
-    $metrics_server_port     = $metrics_server_info['port']
-    $metrics_server_type     = $metrics_server_info['metrics_server_type']
-    $metrics_server_db       = $metrics_server_info['db_name']
+    if !empty($metrics_server_info) {
+      $metrics_server_hostname = $metrics_server_info['hostname']
+      $metrics_server_port     = $metrics_server_info['port']
+      $metrics_server_type     = $metrics_server_info['metrics_server_type']
+      $metrics_server_db       = $metrics_server_info['db_name']
 
-    if empty($metrics_server_db) and $metrics_server_type == 'influxdb'  {
-      fail( 'When using an influxdb server you must provide the db_name to store metrics in' )
-    }
+      if empty($metrics_server_db) and $metrics_server_type == 'influxdb'  {
+        fail( 'When using an influxdb server you must provide the db_name to store metrics in' )
+      }
 
-    $local_metrics_command = "${metrics_base_command} | ${conversion_script_file_name} --netcat ${metrics_server_hostname} --convert-to ${metrics_server_type}"
+      $local_metrics_command = "${metrics_base_command} | ${conversion_script_file_name} --netcat ${metrics_server_hostname} --convert-to ${metrics_server_type}"
 
-    $port_metrics_command = empty($metrics_server_port) ? {
-      false => "${local_metrics_command} --port ${metrics_server_port}",
-      true  => $local_metrics_command,
-    }
+      $port_metrics_command = empty($metrics_server_port) ? {
+        false => "${local_metrics_command} --port ${metrics_server_port}",
+        true  => $local_metrics_command,
+      }
 
-    $metrics_command = $metrics_server_type ? {
-      'influxdb' => "${port_metrics_command} --influx-db ${metrics_server_db} > /dev/null",
-      'graphite' => "${port_metrics_command} > /dev/null",
-      default    => "${port_metrics_command} > /dev/null",
+      $metrics_command = $metrics_server_type ? {
+        'influxdb' => "${port_metrics_command} --influx-db ${metrics_server_db} > /dev/null",
+        'graphite' => "${port_metrics_command} > /dev/null",
+        default    => "${port_metrics_command} > /dev/null",
+      }
+    } else {
+      $metrics_command = "${metrics_base_command} --no-print"
     }
   } else {
-    $metrics_command = "${metrics_base_command} --no-print"
+    $metrics_command = $override_metrics_command
   }
 
   cron { "${metrics_type}_metrics_collection" :
