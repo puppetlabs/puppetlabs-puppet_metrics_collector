8e593ad554eab7a652dcb5f13fb1596ce99db3b6
Jarret Lavallee <jarret.lavallee@puppet.com>
2019-11-05 16:33:43 -0700

    Ensure OSP compatibility with excludes
    Prior to this commit, if the `pe_server_version` fact was missing the
    code would error out. This commit enables OSP and older versions of PE.

--- functions/version_based_excludes.pp
+++ functions/version_based_excludes.pp
@@ -3,7 +3,7 @@ function puppet_metrics_collector::version_based_excludes(
 ) >> Array[String] {
   case $metrics_type {
     'puppetserver': {
-      $excludes = versioncmp($facts['pe_server_version'], '2017.3.0') >= 0 ? {
+      $excludes = ($facts['pe_server_version'] =~ NotUndef and versioncmp($facts['pe_server_version'], '2017.3.0') >= 0) ? {
         true    => ['file-sync-storage-service','pe-puppet-profiler','pe-master','pe-jruby-metrics'],
         default => ['file-sync-storage-service'],
       }
