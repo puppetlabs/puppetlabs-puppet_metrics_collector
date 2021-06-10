064f0fd7bdd5decf426d576cd1f1961a949cbed2
martyewings <martyewings@gmail.com>
2021-06-10 15:21:28 +0100

    fixed spec helper

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,12 +1,3 @@
 # frozen_string_literal: true
 
 require 'singleton'
-
-class Helper
-  include Singleton
-  include PuppetLitmus
-end
-
-def some_helper_method
-  Helper.instance.bolt_run_script('path/to/file')
-end
