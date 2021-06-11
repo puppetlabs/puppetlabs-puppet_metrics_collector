a41589ee6fec9919d8a8670f07e2b88da7382991
martyewings <martyewings@gmail.com>
2021-06-11 11:29:20 +0100

    add have_systemd? test

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper_acceptance'
 
+def have_systemd?
+  host_inventory['facter']['puppet_metrics_collector']['have_systemd'] == 'true'
+end
+
 describe 'puppet_metrics_collector class' do
   context 'activates module default parameters' do
-    it 'sets up crons + timers' do
+    it 'applies the class with default parameters' do
       pp = <<-MANIFEST
         include puppet_metrics_collector
         MANIFEST
@@ -11,6 +15,11 @@ describe 'puppet_metrics_collector class' do
       expect(apply_manifest(pp).exit_code).not_to eq(1)
       expect(apply_manifest(pp).exit_code).not_to eq(1)
       idempotent_apply(pp)
+    end
+    describe 'check systemd fact' do
+      it 'is true on all supported OS' do
+        have_systemd?
+      end
     end
   end
 end
