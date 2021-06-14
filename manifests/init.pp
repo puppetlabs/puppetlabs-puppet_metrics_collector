ab20bf90ecda7caa0b488e7f102bb3e6ca05c0e1
Adrian <sensei.loafage@gmail.com>
2021-06-14 10:36:00 -0700

    Remove collection of AMQ metrics
    This commit removes the files and class declaration for collecting
    ActiveMQ metrics.  It leaves the parameters in the main class definition
    to avoid breaking upgrades if set.

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,8 +18,9 @@ class puppet_metrics_collector (
   String                  $bolt_metrics_ensure         = 'present',
   Array[String]           $bolt_hosts                  = puppet_metrics_collector::hosts_with_pe_profile('bolt_server'),
   Integer                 $bolt_port                   = 62658,
+  # Collection of ActiveMQ metrics has been removed, but the parameters are left to avoid breaking upgrades
   String                  $activemq_metrics_ensure     = 'absent',
-  Array[String]           $activemq_hosts              = puppet_metrics_collector::hosts_with_pe_profile('amq::broker'),
+  Array[String]           $activemq_hosts              = [],
   Integer                 $activemq_port               = 8161,
   Optional[String]        $override_metrics_command    = undef,
   Optional[Array[String]] $puppetserver_excludes       = undef,
@@ -36,82 +37,86 @@ class puppet_metrics_collector (
   $config_dir  = "${output_dir}/config"
   $scripts_dir = "${output_dir}/scripts"
 
-  # If the puppet_metrics_collector::system class is evaluted first,
-  # File[$output_dir] will already be defined along with common scripts.
-  if !defined(File[$output_dir]) {
-    file { [$output_dir, $scripts_dir]:
+    # If the puppet_metrics_collector::system class is evaluted first,
+    # File[$output_dir] will already be defined along with common scripts.
+    if !defined(File[$output_dir]) {
+      file { [$output_dir, $scripts_dir]:
+        ensure => directory,
+      }
+
+      file { "${scripts_dir}/create-metrics-archive":
+        ensure => file,
+        mode   => '0755',
+        source => 'puppet:///modules/puppet_metrics_collector/create-metrics-archive'
+      }
+
+      file { "${scripts_dir}/metrics_tidy":
+        ensure => file,
+        mode   => '0744',
+        source => 'puppet:///modules/puppet_metrics_collector/metrics_tidy'
+      }
+    }
+
+    file { $config_dir:
       ensure => directory,
     }
 
-    file { "${scripts_dir}/create-metrics-archive":
+    file { "${scripts_dir}/json2timeseriesdb" :
       ensure => file,
       mode   => '0755',
-      source => 'puppet:///modules/puppet_metrics_collector/create-metrics-archive'
+      source => 'puppet:///modules/puppet_metrics_collector/json2timeseriesdb'
     }
 
-    file { "${scripts_dir}/metrics_tidy":
+    file { "${scripts_dir}/pe_metrics.rb" :
       ensure => file,
-      mode   => '0744',
-      source => 'puppet:///modules/puppet_metrics_collector/metrics_tidy'
+      mode   => '0755',
+      source => 'puppet:///modules/puppet_metrics_collector/pe_metrics.rb'
+    }
+
+    file { "${scripts_dir}/puma_metrics" :
+      ensure => file,
+      mode   => '0755',
+      source => 'puppet:///modules/puppet_metrics_collector/puma_metrics'
+    }
+
+    file { "${scripts_dir}/tk_metrics" :
+      ensure => file,
+      mode   => '0755',
+      source => 'puppet:///modules/puppet_metrics_collector/tk_metrics'
+    }
+
+    include puppet_metrics_collector::service::puppetserver
+    include puppet_metrics_collector::service::puppetdb
+    include puppet_metrics_collector::service::orchestrator
+    include puppet_metrics_collector::service::ace
+    include puppet_metrics_collector::service::bolt
+
+    # LEGACY CLEANUP
+
+    # Clean up old metrics directories created by the module before it was renamed.
+
+    $legacy_dir      = '/opt/puppetlabs/pe_metric_curl_cron_jobs'
+    $safe_output_dir = shellquote($output_dir)
+
+    exec { "migrate ${legacy_dir} directory":
+      path    => '/bin:/usr/bin',
+      command => "mv ${legacy_dir} ${safe_output_dir}",
+      onlyif  => "[ ! -e ${safe_output_dir} -a -e ${legacy_dir} ]",
+      before  => File[$output_dir],
+    }
+
+    $legacy_files = [
+      '/opt/puppetlabs/bin/puppet-metrics-collector',
+      '/opt/puppetlabs/puppet-metrics-collector/bin',
+    ]
+
+    file { $legacy_files :
+      ensure => absent,
+      force  => true,
+    }
+
+    # Manual cleanup of deprecated AMQ jobs
+    cron { ['activemq_metrics_collection', 'activemq_metrics_tidy']:
+      ensure => absent,
     }
   }
-
-  file { $config_dir:
-    ensure => directory,
-  }
-
-  file { "${scripts_dir}/json2timeseriesdb" :
-    ensure => file,
-    mode   => '0755',
-    source => 'puppet:///modules/puppet_metrics_collector/json2timeseriesdb'
-  }
-
-  file { "${scripts_dir}/pe_metrics.rb" :
-    ensure => file,
-    mode   => '0755',
-    source => 'puppet:///modules/puppet_metrics_collector/pe_metrics.rb'
-  }
-
-  file { "${scripts_dir}/puma_metrics" :
-    ensure => file,
-    mode   => '0755',
-    source => 'puppet:///modules/puppet_metrics_collector/puma_metrics'
-  }
-
-  file { "${scripts_dir}/tk_metrics" :
-    ensure => file,
-    mode   => '0755',
-    source => 'puppet:///modules/puppet_metrics_collector/tk_metrics'
-  }
-
-  include puppet_metrics_collector::service::puppetserver
-  include puppet_metrics_collector::service::puppetdb
-  include puppet_metrics_collector::service::orchestrator
-  include puppet_metrics_collector::service::ace
-  include puppet_metrics_collector::service::bolt
-  include puppet_metrics_collector::service::activemq
-
-  # LEGACY CLEANUP
-
-  # Clean up old metrics directories created by the module before it was renamed.
-
-  $legacy_dir      = '/opt/puppetlabs/pe_metric_curl_cron_jobs'
-  $safe_output_dir = shellquote($output_dir)
-
-  exec { "migrate ${legacy_dir} directory":
-    path    => '/bin:/usr/bin',
-    command => "mv ${legacy_dir} ${safe_output_dir}",
-    onlyif  => "[ ! -e ${safe_output_dir} -a -e ${legacy_dir} ]",
-    before  => File[$output_dir],
-  }
-
-  $legacy_files = [
-    '/opt/puppetlabs/bin/puppet-metrics-collector',
-    '/opt/puppetlabs/puppet-metrics-collector/bin',
-  ]
-
-  file { $legacy_files :
-    ensure => absent,
-    force  => true,
-  }
-}
