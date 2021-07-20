a689aa14b9bbf4b3173e73ad8714037ad24f24a7
Nick Burgan-Illig <nick.burgan-illig@puppet.com>
2021-07-20 17:21:03 +0000

    (maint) Change default for hosts_with_pe_profile when storeconfigs is false
    Previously, this would set the hosts to ['127.0.0.1'] when storeconfigs was false. This would cause flip flops when running puppet infra configure -> puppet agent -t.  This sets the default to the host certname, since this is what the value will end up being on the primary post-install, preventing this flip flop.

--- spec/acceptance/functions/hosts_with_pe_profile_spec.rb
+++ spec/acceptance/functions/hosts_with_pe_profile_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper_acceptance'
 
 describe 'hosts_with_pe_profile function' do
-  it 'returns 127.0.0.1 when no hosts are found' do
+  it 'returns the certname of the node when no hosts are found' do
     pp = <<-MANIFEST
        $result=puppet_metrics_collector::hosts_with_pe_profile('non_existant')
        notice "Received=$result"
        MANIFEST
     output = apply_manifest(pp).stdout
-    expect(output).to match(%r{Received=\[127.0.0.1\]})
+    expect(output).to match(%r{Received=\[#{host_inventory['fqdn']}\]})
   end
+
   it 'return the FQDN for the master profile' do
     # This influences the custom facts
     run_shell('puppet config set storeconfigs true --section user', expect_failures: false)
@@ -19,5 +20,16 @@ describe 'hosts_with_pe_profile function' do
     output = apply_manifest(pp).stdout
     expect(output).to match(%r{Received=\[#{host_inventory['fqdn']}\]})
     run_shell('puppet config set storeconfigs false --section user', expect_failures: false)
+  end
+
+  it 'returns the certname of the node when storeconfigs is false' do
+    # Should be set to false in the previous section
+    # In this case, the certname == fqdn, so we use that here
+    pp = <<-MANIFEST
+       $result=puppet_metrics_collector::hosts_with_pe_profile('master')
+       notice "Received=$result"
+       MANIFEST
+    output = apply_manifest(pp).stdout
+    expect(output).to match(%r{Received=\[#{host_inventory['fqdn']}\]})
   end
 end
