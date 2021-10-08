2a5269a1ba3d753a2c35f018dee8130a1b4d5754
martyewings <martyewings@gmail.com>
2021-10-08 14:49:26 +0100

    Testing dynamic PE version in latest

---
+++ spec/fixtures/matrix/lts.sh
@@ -0,0 +1,1 @@
+curl https://forge.puppet.com/private/versions/pe |  sed -E -e 's/(release")/\n\1/g' | grep "\"lts\":true" |grep -o -P '.{0,0}latest.{0,13}' | awk '{split($0,a,":"); print a[2]}' |  grep -o '".*"' | sed 's/"//g' | head -n 1
