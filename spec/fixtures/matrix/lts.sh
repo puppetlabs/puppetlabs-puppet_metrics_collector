7e6f0f7b563eb37896684639fdd06a821767a7f9
martyewings <martyewings@gmail.com>
2021-10-14 13:53:51 +0100

    fix shell shebang

--- spec/fixtures/matrix/lts.sh
+++ spec/fixtures/matrix/lts.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 curl https://forge.puppet.com/private/versions/pe |  sed -E -e 's/(release")/\n\1/g' | grep "\"lts\":true" |grep -o -P '.{0,0}latest.{0,13}' | awk '{split($0,a,":"); print a[2]}' |  grep -o '".*"' | sed 's/"//g' | head -n 1
